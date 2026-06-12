ca1ab64ae5ea95340b4b1af60753bb235d0cf68f
Uday Surya P <udaysuryap.workspace@gmail.com>
2026-06-12 11:25:43 +0530

    Day 02

--- Patterns.py
+++ Patterns.py
@@ -1,32 +1,49 @@
-print("Pattern 1")
-n = int(input("Enter the number of rows: "))
-for i in range(n):
-    for j in range(5):
-        print("*", end="")
-    print(end="\n")    
+# print("Pattern 1")
+# n = int(input("Enter the number of rows: "))
+# for i in range(n):
+#     for j in range(5):
+#         print("*", end="")
+#     print(end="\n")    
 
 
 
-print("Pattern 2")
-n = int(input("Enter the number of rows: "))
-for i in range(n+1):
+# print("Pattern 2")
+# n = int(input("Enter the number of rows: "))
+# for i in range(n+1):
+#     for j in range(i):
+#         print("*", end="")
+#     print(end="\n")    
+
+
+# print("Pattern 3")
+# n = int(input("Enter the number of rows: "))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(j+1, end="")
+#     print(end="\n")    
+
+
+# print("Pattern 4")
+# n = int(input("Enter the number of rows: "))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(i, end="")
+#     print(end="\n")    
+
+print("Pattern 5")
+i = int(input("Enter the number of rows:"))
+
+while i>0:
     for j in range(i):
         print("*", end="")
-    print(end="\n")    
+    print(end="\n")
+    i-=1
 
+print("Pattern 5")
+i = int(input("Enter the number of rows:"))
 
-print("Pattern 3")
-n = int(input("Enter the number of rows: "))
-for i in range(n+1):
+while i>0:
     for j in range(i):
         print(j+1, end="")
-    print(end="\n")    
-
-
-print("Pattern 4")
-n = int(input("Enter the number of rows: "))
-for i in range(n+1):
-    for j in range(i):
-        print(i, end="")
-    print(end="\n")    
-
+    print(end="\n")
+    i-=1
